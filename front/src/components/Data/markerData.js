6cebe86cff5fe624479a8eeaedb69f109294555e
JunYoungKr <kop981020@naver.com>
2023-09-14 15:22:58 +0900

    [add]Overlay 수정 및 review 페이지 디자인

--- front/src/components/Data/markerData.js
+++ front/src/components/Data/markerData.js
@@ -1,18 +1,12 @@
-import React from "react";
-
 export const markerdata = [
   {
     title: "제주몰빵",
     lat: 37.549321,
     lng: 127.075044,
-    overlayContent:
-      '<div style="background-color: white; border: 1px solid black; padding: 5px;"><div><span>제주몰빵</span><hr><div>잔여 좌석 1 / 45<div>평점 4.5 / 5.0</div></div><button>자세히 알아보기 >> </button></div></div>',
   },
   {
     title: "팬도로시",
     lat: 37.549809,
     lng: 127.075371,
-    overlayContent:
-      '<div style="background-color: white; border: 1px solid black; padding: 5px;"><div><span>팬도로시</div></div>',
   },
 ];
