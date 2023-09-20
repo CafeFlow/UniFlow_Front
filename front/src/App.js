e58f0dad830a2d98a9cddd53f5a5f7ea956f8d71
JunYoungKr <kop981020@naver.com>
2023-09-20 19:28:48 +0900

    [add]Google Analytics 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga";
+
 import Home from "./components/Home/Home";
 import Review from "./components/Review/Review";
 import Header from "./components/Header/Header";
+
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
+ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
+ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
 
 const App = () => {
   return (
