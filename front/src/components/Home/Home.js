747586d073588cf2462ce5290af24950af4747ee
JunYoungKr <kop981020@naver.com>
2023-10-10 21:46:24 +0900

    test

--- front/src/components/Home/Home.js
+++ front/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { API_URL } from "../constant";
 import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
 import seatGreen from "../icons/seatGreen.png";
@@ -27,8 +28,7 @@ const Home = ({
     lng: 127.074055,
   });
 
-  const API_URL = process.env.REACT_APP_API_URL;
-
+  const API = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
 
   const navigateToReview = () => {
@@ -131,7 +131,7 @@ const Home = ({
 
     // axios로 가져온 데이터로 마커 생성
     axios
-      .get(`${API_URL}/view-map`)
+      .get(`${API}/view-map`)
       .then((response) => {
         const data = response.data;
         console.log(data);
